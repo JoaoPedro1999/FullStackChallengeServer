4a5d8d6c6dcfc952a1329782fd11fa02c02c6961
João Pedro Beck Land <joao.land@corebiz.ag>
2022-08-02 21:39:02 -0300

    feat: initial route

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response, NextFunction } from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('🚀 server started on port 3333');
